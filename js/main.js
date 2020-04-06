9b188501902ea2c2a0f2f5321828ee63f8d40a4f
José Vitor <jose-vitor-7@hotmail.com>
2020-04-06 20:04:15 -0300

    Adicionando estilização

--- js/main.js
+++ js/main.js
@@ -15,30 +15,28 @@ let food = { // posição aleatória da comida
 
 
 const criarBackground = () => {
-    context.fillStyle = 'lightgreen'; // Cor que será usada no fundo canvas
+    context.fillStyle = '#262626'; // Cor que será usada no fundo canvas
     context.fillRect(0, 0, 16 * box, 16 * box); // Desenha o canvas, (x, y, altura, largura) de cada quadrado -> 16 * 32 = 512
 };
 
 const criarSnake = () => {
     for ( let i = 0; i < snake.length; i ++ ){
-        context.fillStyle = 'green'; // Cor que será usada na cobrinha
+        context.shadowBlur = 30;
+        context.shadowColor = 'green';
+        context.fillStyle = 'rgb(0, 255, 13)'; // Cor que será usada na cobrinha
         context.fillRect(snake[i].x, snake[i].y, box, box); // Desenha a cobrinha de acordo com o tamanho do vetor snake
+        
     }
 }
 
 const criarComida = () => {
-    context.fillStyle = 'red';
+    context.shadowBlur = 30;
+    context.shadowColor = 'magenta';
+    context.fillStyle = 'rgb(241, 0, 141)';
     context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', (event) => { // "escuta" o evento de apertar uma tecla
-    for( let i = 1; i < snake.length; i++ ){
-        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){ // se a "cabeça" da cobrinha se colidir com qualquer outra parte dela mesmo, o jogo finaliza
-            clearInterval(jogo);
-            alert('Game Over :[');
-        }
-    }
-
     // todas as teclas definidas e faz com que não possa voltar para a direção inversa no mesmo eixo -> se estou para a direita, não posso ir para esquerda; se estou para cima, não posso ir para baixo.
     if ( event.keyCode == 39 && direction !== 'left' ) direction = 'right';
     if ( event.keyCode == 37 && direction !== 'right' ) direction = 'left';
@@ -53,6 +51,13 @@ const iniciarJogo = () => {
     if ( snake[0].x > 15 * box && direction === 'right' ) snake[0].x = 0;
     if ( snake[0].y < 0 * box && direction === 'up' ) snake[0].y = 16 * box;
     if ( snake[0].y > 15 * box && direction === 'down' ) snake[0].y = 0;
+
+    for( let i = 1; i < snake.length; i++ ){
+        if ( snake[0].x == snake[i].x && snake[0].y == snake[i].y ){ // se a "cabeça" da cobrinha se colidir com qualquer outra parte dela mesmo, o jogo finaliza
+            clearInterval(jogo);
+            alert('Game Over :[');
+        }
+    }
 
     criarBackground(); // Executa a função de criar o fundo do canvas
     criarSnake(); // Executa a função de criar a cobrinha
@@ -78,6 +83,7 @@ const iniciarJogo = () => {
     };
 
     snake.unshift(newHead); // adiciona uma nova "cabeça" ao topo do corpo da cobrinha
+    console.log(snake[0], snake[1]);
 }
 
-let jogo = setInterval(iniciarJogo, 100);
+let jogo = setInterval(iniciarJogo, 75);
