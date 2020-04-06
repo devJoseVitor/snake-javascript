06631c18880072e99d9c9f25a06846a50db8c355
José Vitor <jose-vitor-7@hotmail.com>
2020-04-06 17:45:47 -0300

    Definindo movimentos da cobrinha

--- js/main.js
+++ js/main.js
@@ -7,18 +7,41 @@ let snake = [ // posição inicial
         y : 8 * box
     }
 ];
+let direction = 'up';
+
 
 const criarBackground = () => {
     context.fillStyle = 'lightgreen'; // Cor que será usada no fundo canvas
-    context.fillRect(0, 0, 16 * box, 16 * box); // Desenha o canvas, (x, y, altura, largura) de cada pixel -> 16 * 32 = 512
+    context.fillRect(0, 0, 16 * box, 16 * box); // Desenha o canvas, (x, y, altura, largura) de cada quadrado -> 16 * 32 = 512
 };
 
 const criarSnake = () => {
-    for ( let i = 0; i < snake.length; i ++){
+    for ( let i = 0; i < snake.length; i ++ ){
         context.fillStyle = 'green'; // Cor que será usada na cobrinha
         context.fillRect(snake[i].x, snake[i].y, box, box); // Desenha a cobrinha de acordo com o tamanho do vetor snake
     }
 }
 
-criarBackground(); // Executa a função de criar o fundo do canvas
-criarSnake(); // Executa a função de criar a cobrinha
+const iniciarJogo = () => {
+    criarBackground(); // Executa a função de criar o fundo do canvas
+    criarSnake(); // Executa a função de criar a cobrinha
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if ( direction === 'right' ) snakeX += box; // se for para direita, adiciona um quadrado para o lado direito
+    if ( direction === 'left' ) snakeX -= box; // se for para esquerda, adiciona um quadrado para o lado esquerdo
+    if ( direction === 'up' ) snakeY -= box; // se for para cima, adiciona um quadrado para o lado cima
+    if ( direction === 'down' ) snakeY += box; // se for para baixo, adiciona um quadrado para o lado baixo
+
+    snake.pop(); // remove a última "cabeça" do corpo da cobrinha
+
+    let newHead = { // define onde deve aparecer a nova "cabeça" da cobrinha
+        x : snakeX,
+        y : snakeY
+    };
+
+    snake.unshift(newHead); // adiciona uma nova "cabeça" ao topo do corpo da cobrinha
+}
+
+let jogo = setInterval(iniciarJogo, 100);
